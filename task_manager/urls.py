e2ac79e9b6bd601e1d87deca0ced1c235e94427a
Anna Bystrova <annbystr@gmail.com>
2023-09-20 12:28:46 +0300

    add labels app

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -28,5 +28,6 @@ urlpatterns = i18n_patterns(
     path("users/", include("task_manager.users.urls")),
     path("statuses/", include("task_manager.statuses.urls")),
     path("task/", include("task_manager.tasks.urls")),
+    path("labels/", include("task_manager.labels.urls")),
     path("admin/", admin.site.urls),
 )
